79072af5f6a5cc7596b4b34c59755ef138d8b1d1
CHRISTOPHER <chris@sitevitals.net>
2020-01-25 23:15:14 +1300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -106,14 +106,3 @@ const updateUI = async () => {
   document.getElementById("btn-login").disabled = isAuthenticated;
 };
 
-const login = async () => {
-  await auth0.loginWithRedirect({
-    redirect_uri: window.location.origin
-  });
-};
-
-const logout = () => {
-  auth0.logout({
-    returnTo: window.location.origin
-  });
-};
